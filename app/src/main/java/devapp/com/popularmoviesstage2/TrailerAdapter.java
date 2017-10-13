af84788ac07ae51c3f9bd055c0147eb233669255
Deven <deven9852@gmail.com>
2017-10-13 08:07:46 +0530

    TrailerAdapter Completed

--- app/src/main/java/devapp/com/popularmoviesstage2/TrailerAdapter.java
+++ app/src/main/java/devapp/com/popularmoviesstage2/TrailerAdapter.java
@@ -1,8 +1,51 @@
 package devapp.com.popularmoviesstage2;
 
-/**
- * Created by HP on 13-10-2017.
- */
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class TrailerAdapter {
+
+
+public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.TrailerViewHolder> {
+
+    Context mContext;
+
+    TrailerAdapter(Context mContext){
+
+        this.mContext = mContext;
+
+    }
+
+    @Override
+    public TrailerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new TrailerViewHolder(LayoutInflater.from(mContext).inflate(R.layout.trailer_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(TrailerViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return DetailActivity.trailerIDs.size();
+    }
+
+    public class TrailerViewHolder extends RecyclerView.ViewHolder{
+
+        TextView trailerNumberTextView;
+
+        public TrailerViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        void bind(int position){
+            trailerNumberTextView.setText("Trailer " + position+1);
+        }
+
+    }
+
 }
